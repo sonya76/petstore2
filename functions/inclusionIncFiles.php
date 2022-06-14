11ee372357ac928996706c3fa06752a0a06de6e5
unknown <bouazzicdi@gmail.com>
2022-06-14 21:15:46 +0200

    ajout de dossier function et aussi la fonction inclusionIncFile pour switcher entre les page

---
+++ functions/inclusionIncFiles.php
@@ -0,0 +1,14 @@
+<?php
+
+function inclusionIncFile(string $defaultPage): void
+{
+    $files = glob('./includes/*.inc.php');
+    $page = $_GET['page'] ?? $defaultPage;
+
+    $pageTest = './includes/' . $page . '.inc.php';
+
+    if (!in_array($pageTest, $files)) 
+        require './includes/' . $defaultPage . '.inc.php';
+    else
+        require $pageTest;
+}
