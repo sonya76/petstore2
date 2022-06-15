9d2c7b7410db3dc284c88e2f87cdbfdc81d8f776
Sonia Chalal <contact@soniawebdesigner.fr>
2022-06-15 10:30:22 +0200

    modification page connexion en html seule

--- includes/connexion.inc.php
+++ includes/connexion.inc.php
@@ -1,1 +1,46 @@
-<h1>Connexion</h1>
+    <div class="hearderConnexion">
+        <h1>Connexion</h1>
+    </div>
+    <div class="box">
+        <div class="row">
+            <div class="column">
+                <div class="left-column">
+                    <h3>J'ai un compte <span class="logoTitre">PetStore</span></h3>
+                    <form action="" method="post">
+                        <div class="form-group">
+                        <label for="email">Email </label>
+                        <input type="text" name="email" required />
+                        </div>
+                        <div class="form-group">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" name="password" required />
+                        </div>
+                        <div class="form-group">
+                        <a class="passwordOublie" href="#">Mot de passe oublié ?</a>
+                        </div>
+                        <div class="form-group">
+                        <input type="submit" value="Connexion" id="submitConnex" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="column">
+                <div class="right-column">
+                    <h3 class="nouveauCompte"> Je crée mon compte <span class="logoTitre">PetStore</span></h3>
+                <p><i>L'inscription est simple et gratuite !</i></p>
+                <div class="form-group">
+                        <p>
+                            <ul class="listePrivileges">
+                                <li> Consulter vos points et remises de votre carte de fidélité <span class="logoTitre">PetStore</span></li>
+                                <li> Gérez vos informations personnelles</li>
+                                <li> Suivez vos commandes passées en magasin ou en ligne</li>
+                                <li> et encore de nombreux avantages...</li>
+                            </ul>
+                        </p>
+                    <div class="form-group">
+                        <input type="submit" value="Créer un compte" id="submit" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
