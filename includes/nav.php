a2a5107c68a7e450a60b9d96e85dbc0b4bfb3a62
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-15 11:59:51 +0200

    html nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,40 @@
 <header>
-    <nav>
-        <ul>
-        <li><a href="index.php?page=accueil">Accueil</a></li>
-         <li><a href="index.php?page=inscription">Inscription</a></li>
-                <li><a href="index.php?page=connexion">Connexion</a></li>
-                <li><a href="index.php?page=contact">Contact</a></li>
-              </ul>
+    <nav id="navp">
+      <a href="index.php?page=accueil"><img src="./assets/img/logo1.png" alt="logo de petstore  " /></a>
+      <ul id="headMenu">
+        <div class="container"><input type="search" placeholder="Rechercher ..."/><button type="submit">Search</button></div>
+        <li>
+          <a href="index.php?page=accueil">
+            <div class="icon"><i class="fa-solid fa-house fa-2xl"></i></div>
+            <div><span>Home</span></div>
+          </a>
+        </li>
+        <li>
+          <a href="index.php?page=inscription">
+            <div class="icon"><i class="fa-solid fa-user fa-2xl"></i></div>
+            <div><span>Inscription</span></div>
+          </a>
+        </li>
+        <li>
+          <a href="index.php?page=connexion">
+            <div class="icon"><i class="fa-solid fa-user fa-2xl"></i></div>
+            <div><span>Connexion</span></div>
+          </a>
+        </li>
+        <li>
+          <a href="index.php?page=contact">
+            <div class="icon"><i class="fa-solid fa-phone fa-2xl"></i></div>
+            <div><span>Contact</span></div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div class="icon">
+              <i class="fa-solid fa-basket-shopping fa-2xl"></i>
+            </div>
+            <div><span>Panier</span></div>
+          </a>
+        </li>
+      </ul>
     </nav>
 </header>
