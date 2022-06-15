3417cbb541e96004f8836157cbec01589c1ba87e
noraceppic <messanora86@gmail.com>
2022-06-15 14:08:47 +0200

    ajout de fichier frmContact2

---
+++ includes/frmContact2.php
@@ -0,0 +1,101 @@
+<form action="index.php?page=contact" method="post">
+
+<main>
+        <div class="headerContact">
+            <h1>
+                Nous contacter
+            </h1>
+
+        </div>
+
+
+
+        <div class="box">
+           
+            
+                    <div class="left-column">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="Nom"> Nom :</label>
+                                <input type="text">
+                            </div>
+                            <div class="form-group">
+                                <label for="Email ">Email:</label>
+                                <input type="text">
+                            </div>
+                            <div class="form-group">
+                                <label for="Numéro de télephone">Numéro de téléphone: </label>
+                                <input type="text">
+                            </div>
+                            <div class="form-group">
+                                <label for="msg">Comment pouvons-nous vous aider ? : </label>
+                                <textarea id="msg" name="user_message"></textarea>
+
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value="Envoyer" id="submit">
+
+
+                            </div>
+                        </form>
+                    </div>
+
+
+                
+                <div= class="right-column">
+                    <ul id="right-column">
+
+                        
+                        <li>
+                            <div class="icone"><i class="fa-solid fa-location-dot"></i>  Address:  194 Rue de la Ronce, 76230 Isneauville </div>
+                        </li>
+                        <li>
+                            <div class="icone"><i class="fa-solid fa-phone"></i> Téléphone: 07 50 50 50 50 </div>
+                        </li>
+                        <li>
+                            <div class="icone"><i class="fa-solid fa-envelope-circle-check"></i>  Email: contact@petstor.com</div>
+                        </li>
+
+                    </ul>
+                    
+                    <ul id="reseauxSociaux">
+                        
+                        <a href="#">
+                        <li>
+                            <div class="iconeReseaux"><i class="fa-brands fa-linkedin  fa-2xl"></i> </div>
+
+                        </li>
+                       </a>
+                       <a href="#">
+                        <li>
+                            <div class="iconeReseaux"><i class="fa-brands fa-facebook-square  fa-2xl"></i> </div>
+
+                        </li>
+                       </a>
+                       <a href="#">
+                        <li>
+                            <div class="iconeReseaux"><i class="fa-brands fa-twitter fa-2xl"></i></div>
+
+                        </li>
+                      </a>
+                      <a href="#">
+                        <li>
+                            <div class="iconeReseaux"><i class="fa-brands fa-instagram fa-2xl"></i> </div>
+
+                        </li>
+                         </a>
+
+                    </ul>
+                    
+            
+                </div>
+        </div>
+
+
+
+
+
+
+
+    </main>
+    </form>
