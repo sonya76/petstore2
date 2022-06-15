067ddcacd806a1876142a81a6e789df1c5a3491e
BouazziH <bouazzicdi@gmail.com>
2022-06-15 17:04:59 +0200

    ajout des icones et de code html

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,51 @@
+<link rel="stylesheet" href="../assets/css/style.css">
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> - PETSTORE GROUPE 2 juste pour le test</p>
+    <div class="contenaire">
+<div class="haut">
+<div class="icone"><div><i class="fa-solid fa-box-open fa-2xl"></i><span>Livraison en 2 à 5 jours ouvrés</span> </div></div>
+<div class="icone"><div><i class="fa-solid fa-store fa-2xl"></i><span>Click & collect sous 2h en magasin</span> </div></div>
+<div class="icone"><div><i class="fa-solid fa-truck-fast fa-2xl"></i><span>Livraison offerte dès 49€</span> </div></div>
+<div class="icone"><div><i class="fa-solid fa-shield fa-2xl"></i><span>Paiement sécurisé</span> </div></div>
+</div>
+<div class="bas">
+<div>
+    <p><h3>PetStore</h3></p>
+    <p>une petite description toto</p>
+    <p>194 Rue de la Ronce, 76230 Isneauville</p>
+    <p>07 10 12 25 52</p>
+    <p>contact@petstore.com</p>
+    <div><!--pour traiter les icones social medias -->
+        <i class="fa-brands fa-instagram fa-xl a"></i>
+    <i class="fa-brands fa-facebook-square fa-xl a"></i>
+    <i class="fa-brands fa-snapchat-square fa-xl a"></i>
+</div> 
+</div>
+<div><p>INFORMATIONS</p>
+<p>Qui sommes nous ?</p>
+<p>Découvrir Petsor Santé</p>
+<p>Découvrir mydogmate</p>
+<p>Espace Presse</p>
+<p>CGV</p>
+<p>Crédit</p>
+<p>Mentions légales</p>
+<p>Modes de livraison</p>
+<p>Tous nos magasins</p>
+<p>Toutes nos offres promotionnelles </p></div>
+<div></div>
+<div></div>
+
+
+
+ <p>PetStore &copy; <?php echo date("Y"); ?> - all rights reserved</p>
+</div>
+
+
+
+
+
+
+    </div>
+   
 </footer>
 </body>
 </html>
