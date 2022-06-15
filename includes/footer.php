879a63e6fa0c28808ee8605d4fcfe0e69ed03546
unknown <bouazzicdi@gmail.com>
2022-06-15 23:02:47 +0200

    tout le code html du footer

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,7 @@
 <div class="icone"><div><i class="fa-solid fa-shield fa-2xl"></i><span>Paiement sécurisé</span> </div></div>
 </div>
 <div class="bas">
-<div>
+<div class="a">
     <p><h3>PetStore</h3></p>
     <p>une petite description toto</p>
     <p>194 Rue de la Ronce, 76230 Isneauville</p>
@@ -20,7 +20,8 @@
     <i class="fa-brands fa-snapchat-square fa-xl a"></i>
 </div> 
 </div>
-<div><p>INFORMATIONS</p>
+<div class="b">
+    <p><h4>INFORMATIONS</h4></p>
 <p>Qui sommes nous ?</p>
 <p>Découvrir Petsor Santé</p>
 <p>Découvrir mydogmate</p>
@@ -31,16 +32,46 @@
 <p>Modes de livraison</p>
 <p>Tous nos magasins</p>
 <p>Toutes nos offres promotionnelles </p></div>
-<div></div>
-<div></div>
+<div class="c">
+    <p><h4>CATEGORIES</h4></p>
+    <p>Chiens</p>
+    <p>Chats</p>
+    <p>Aquariophilie & Bassins</p>
+    <p>Oiseaux</p>
+    <p>Rongeurs & Co</p>
+    <p>Reptiles & Co</p>
+    <p>Animaux de la nature</p>
+    <p>Livres, Cadeaux & Art créatif</p>
+    <p>Nos Conseils</p>
+    
+</div>
+<div class="d">
+<p><h4>PETSTORE RECOMMANDE</h4></p>
+<p> PetStore</p>
+<p>Purina One</p>
+<p>Royal Canin</p>
+<p>Nutrivia Nature</p>
+<p>Nutrivia</p>
+<p>Wellnes</p>
+<p>Orijen</p>
+<p>Acana</p>
+<p>Nature Diet</p>
+<p>Lily's Kitchen</p>
+<p>Vigor &Sage</p>
+<p>Carnilove</p>
+<p>Vitakraft</p>
+<p>Tetra</p>
 
 
-
- <p>PetStore &copy; <?php echo date("Y"); ?> - all rights reserved</p>
 </div>
 
 
 
+</div>
+
+ 
+
+<div class="e"><p>PetStore &copy; <?php echo date("Y"); ?> - all rights reserved</p></div>
 
 
 
