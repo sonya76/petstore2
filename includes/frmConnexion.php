8eab9b835e43de6cf0263db6adb10c667d0d9cc8
BouazziH <bouazzicdi@gmail.com>
2022-06-16 12:21:04 +0200

    affectation de variable de test

--- includes/frmConnexion.php
+++ includes/frmConnexion.php
@@ -8,14 +8,14 @@
           </div>
           
           <div class="box-left">
-            <form action="" method="post">
+            <form action="index.php?page=connexion" method="post">
               <div class="form-group">
-                <label for="email">Email </label>
-                <input type="text" name="email" required />
+                <label for="mail">Email </label>
+                <input type="email" name="mail" value="<?=$mail?>" />
               </div>
               <div class="form-group">
                 <label for="password">Mot de passe</label>
-                <input type="password" name="password" required />
+                <input type="password" name="password"  />
               </div> 
               <div class="form-group">
                 <a class="passwordOublie" href="#">Mot de passe oublié ?</a>
@@ -23,6 +23,7 @@
               <div class="form-group">
                 <input type="submit" value="Connexion" id="submitConnex" />
               </div>
+              <input type="hidden" name="frmConnexion" />
             </form>
           </div>
         </div>
