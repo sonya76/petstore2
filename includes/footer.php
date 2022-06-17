db464e5131e1dcc85ce3cdc9f6c2a8511db6fd38
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-06-17 17:18:14 +0200

    fix footer à finir

--- includes/footer.php
+++ includes/footer.php
@@ -1,34 +1,39 @@
 <div class="haut">
     <div class="icone">
-        <div><i class="fa-solid fa-box-open fa-4x"></i><span><strong> Livraison en 2 à 5 jours ouvrés</strong></span></div>
+        <div class="label"><i class="fa-solid fa-box-open fa-4x"></i><span><strong> Livraison en 2 à 5 jours ouvrés</strong></span></div>
     </div>
     <div class="icone">
-        <div><i class="fa-solid fa-store fa-4x"></i><span><strong> Click & collect sous 2h en magasin</strong></span></div>
+        <div class="label"><i class="fa-solid fa-store fa-4x"></i><span><strong> Click & collect sous 2h en magasin</strong></span></div>
     </div>
     <div class="icone">
-        <div><i class="fa-solid fa-truck-fast fa-4x"></i><span><strong> Livraison offerte dès 49€</strong></span></div>
+        <div class="label"><i class="fa-solid fa-truck-fast fa-4x"></i><span><strong> Livraison offerte dès 49€</strong></span></div>
     </div>
     <div class="icone">
-        <div><i class="fa-solid fa-shield fa-4x"></i><span><strong> Paiement sécurisé</strong></span></div>
+        <div class="label"><i class="fa-solid fa-shield fa-4x"></i><span><strong> Paiement sécurisé</strong></span></div>
     </div>
 </div>
 <footer class="footer">
 
     <div class="footer__addr">
         <h1 class="footer__logo">PetStore</h1>
+        <p>une petite description toto</p>
         <address>
-            <p>une petite description toto</p>
             <p>194 Rue de la Ronce, 76230 Isneauville</p>
+            <br />
             <p>07 10 12 25 52</p>
+            <br />
             <a href="mailto:contact@petstore.com">contact@petstore.com</a>
         </address>
+        <br />
         <div class="footer__iconesSocial">
             <!--pour traiter les icones social medias -->
+            <a href="#"><i class="fa-brands fa-facebook-square"></i></a>
+            <a href="#"><i class="fa-brands fa-linkedin"></i>
+            <a href="#"><i class="fa-brands fa-instagram-square"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            <a href="#"> <i class="fa-brands fa-snapchat-square"></i></a>
            
-            <a href="#"><i class="fa-brands fa-facebook-square fa-xl a"></i></a>
-            <a href="#"><i class="fa-brands fa fa-linkedin-square fa-xl a"></i></a>
-            <a href="#"><i class="fa-brands fa-instagram fa-xl a"></i></a>
-            <a href="#"><i class="fa-brands fa-youtube fa-xl a"></i></a>
+
         </div>
     </div>
 
@@ -70,10 +75,10 @@
                 </li>
             </ul>
         </li>
-        <li class="nav__item nav__item--extra">
+        <li class="nav__item">
             <h2 class="nav__title">CATEGORIES</h2>
 
-            <ul class="nav__ul nav__ul--extra">
+            <ul class="nav__ul">
                 <li>
                     <a href="#">Chiens</a>
                 </li>
