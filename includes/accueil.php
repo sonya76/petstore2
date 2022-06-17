02aa3d2a94e9cbf3b03feb56a4d99d2061464ee2
noraceppic <messanora86@gmail.com>
2022-06-17 15:25:20 +0200

    ajout icone univers

---
+++ includes/accueil.php
@@ -0,0 +1,28 @@
+<div class="univers">
+    <ul>
+        <li>
+            <a href="">
+                <span class=icone chien chien><i class="fa-solid fa-dog"></i></span>
+                <span class= text-univers></span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <span class=icone chien chat><i class="fa-solid fa-dog"></i></span>
+                <span class= text-univers></span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <span class=icone chien chien><i class="fa-solid fa-dog"></i></span>
+                <span class= text-univers></span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <span class=icone chien chien><i class="fa-solid fa-dog"></i></span>
+                <span class= text-univers></span>
+            </a>
+        </li>
+    </ul>
+</div>
