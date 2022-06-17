bd22d22029a0ea559c6643f0660f366bec7f85f6
noraceppic <messanora86@gmail.com>
2022-06-17 17:11:22 +0200

    ajout des icons animeaux home page

--- includes/accueil.php
+++ includes/accueil.php
@@ -2,27 +2,48 @@
     <ul>
         <li>
             <a href="">
-                <span class=icone chien chien><i class="fa-solid fa-dog"></i></span>
-                <span class= text-univers></span>
+                <span class=icone chien chien><i class=" fa-solid fa-dog "></i></span>
+                <span class= text-univers>Chiens</span>
             </a>
         </li>
         <li>
             <a href="">
-                <span class=icone chien chat><i class="fa-solid fa-dog"></i></span>
-                <span class= text-univers></span>
+                <span class=icone chats><i class=" fa-solid fa-cat"></i></span>
+                <span class= text-univers>Chats</span>
             </a>
         </li>
         <li>
             <a href="">
-                <span class=icone chien chien><i class="fa-solid fa-dog"></i></span>
-                <span class= text-univers></span>
+                <span class=icone poisson><i class="fa-solid fa-fish-fins"></i></span>
+                <span class= text-univers>Poissons</span>
             </a>
         </li>
         <li>
             <a href="">
-                <span class=icone chien chien><i class="fa-solid fa-dog"></i></span>
-                <span class= text-univers></span>
+                <span class=icone oiseaux><i class="fa-solid fa-dove"></i></span>
+                <span class= text-univers>Oiseaux</span>
+            </a>
+            
+        </li>
+        <li><a href="">
+                <span class=icone rongeurs><i class="fa-solid fa-dog"></i></span>
+                <span class= text-univers>Rongeurs & co</span>
+            </a></li>
+        <li>
+        <a href="">
+                <span class=icone reptiles><i class="fa-regular fa-turtle"></i></span>
+                <span class= text-univers>Reptiles & co</span>
             </a>
         </li>
+        <li>
+        <a href="">
+                <span class=icone animaux de la ferme><i class="fa-solid fa-cow"></i></span>
+                <span class= text-univers>Animaux de la ferme</span>
+            </a>
+        </li>
+        <li><a href="">
+                <span class=icone animaux de la nature><i class="fa-solid fa-cow"></i></span>
+                <span class= text-univers>Animeaux du la nature</span>
+            </a></li>
     </ul>
 </div>
