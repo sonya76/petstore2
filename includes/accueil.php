8063d8fd17db7776440e9a446934ed792d04861f
noraceppic <messanora86@gmail.com>
2022-07-14 18:23:54 +0200

    reduire nombre d image du salder

--- includes/accueil.php
+++ includes/accueil.php
@@ -2,73 +2,67 @@
     <ul id =hadUnivres>
         <li>
             <a href="">
-                <span class=icon-univers  chien chien><i class=" fa-solid fa-dog "></i></span>
+                <span class=icon-univers ><i class=" fa-solid fa-dog "></i></span>
                 <span class= text-univers>Chiens</span>
             </a>
         </li>
         <li>
             <a href="">
-                <span class=icon-univers  chats><i class=" fa-solid fa-cat"></i></span>
+                <span class=icon-univers  ><i class=" fa-solid fa-cat"></i></span>
                 <span class= text-univers>Chats</span>
             </a>
         </li>
         <li>
             <a href="">
-                <span class=icon-univers   poisson><i class="fa-solid fa-fish-fins"></i></span>
+                <span class=icon-univers  ><i class="fa-solid fa-fish-fins"></i></span>
                 <span class= text-univers>Poissons</span>
             </a>
         </li>
         <li>
             <a href="">
-                <span class=icon-univers   oiseaux><i class="fa-solid fa-dove"></i></span>
+                <span class=icon-univers  ><i class="fa-solid fa-dove"></i></span>
                 <span class= text-univers>Oiseaux</span>
             </a>
             
         </li>
         <li><a href="">
-                <span class=icon-univers   rongeurs><i class="fa-solid fa-dog"></i></span>
-                <span class= text-univers>Rongeurs & co</span>
+                <span class=icon-univers  ><i class="fa-solid fa-dog"></i></span>
+                <span class= text-univers>Rongeurs </span>
             </a></li>
         <li>
         <a href="">
-                <span class=icon-univers  reptiles><i class="fa-solid fa-worm"></i></span>
-                <span class= text-univers>Reptiles & co</span>
+                <span class=icon-univers  ><i class="fa-solid fa-worm"></i></span>
+                <span class= text-univers>Reptiles </span>
             </a>
         </li>
         <li>
         <a href="">
-                <span class=icon-univers  animaux de la ferme><i class="fa-solid fa-cow"></i></span>
-                <span class= text-univers>Animaux de la ferme</span>
+                <span class=icon-univers  ><i class="fa-solid fa-cow"></i></span>
+                <span class= text-univers>Animaux </span>
             </a>
         </li>
         
     </ul>
 </div>
+<section class=slide>
 <div class="slider">
-    <div class="contunairSlider">
-        <div id="imgSlider">
+    <div class="image">
         <img src="./assets/img/slider1.png" alt="" />
-        </div>
-        <div id="imgSlider">
         <img src="./assets/img/slider2.png" alt="" />
-        </div>
-        <div id="imgSlider">
-       <img src="./assets/img/slider3.png" alt="" />
-        </div>
-        <div id="imgSlider">
-       <img src="./assets/img/slider4.png" alt="" />
-        </div>
-        <div id="imgSlider">
-        <img src="./assets/img/slider5.png" alt="" />
-        </div>
+        <img src="./assets/img/slider3.png" alt="" />
+        <img src="./assets/img/slider4.png" alt="" />
+        <img src="./assets/img/slider1.png" alt="" />
+        
     </div>
-    <div class="directions">
+
+   <!--  <div class="directions">
     <div class="lefet">
         <i>&#139;</i>
     </div>
     <div class="right">
     <i>&#155;</i>
-    </div>
+    </div> -->
    
     
 </div>
+</section>
